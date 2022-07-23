f2ceb4f6e069298b1437712c879d2e0a85ad770f
marcusmors <alivezeh@gmail.com>
2022-07-23 17:21:26 -0500

    moving the project to selenium cause wsl compatibility

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
 # from bs4 import BeautifulSoup
+
+import time
+
 from playwright.sync_api import sync_playwright
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+# from http.server import executable
+# from turtle import ht
+
+
+
+def get_academico_course_weights():
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    }
+
+    academico_login_url: str = "https://academico.ucsp.edu.pe/"
+    academico_principal_url: str = "https://academico.ucsp.edu.pe/principal.html"
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.implicitly_wait(10)
+    driver.get(academico_login_url)
+    driver.find_element(By.XPATH, '//input[@id="txt_usr"]').send_keys(user_info["id"])
+    password_field = driver.find_element(By.XPATH, '//input[@id="txt_pwd"]')
+    password_field.send_keys(user_info["password"])
+    password_field.send_keys(Keys.RETURN)
+
+    driver.switch_to.frame(driver.find_element(By.NAME,"cabecera"))
+    student_button_table = WebDriverWait(driver, 10).until(
+    expected_conditions.presence_of_element_located((By.ID, 'mnu_principaln1'))
+    )
+    student_button = student_button_table.find_element(By.TAG_NAME, 'a')
+    student_button.click()
+
+    print("successful LOG IN")
+    html = driver.page_source
+    print(html)
+    driver.close()
+
 
 
 def main():
-    virtual_ucsp_login_url: str = "https://accounts.google.com/o/oauth2/v2/auth/identifier?client_id=95208330785-4d7086dmtrllm99c38m6bv0dkvjrm9ot.apps.googleusercontent.com&response_type=code&redirect_uri=https%3A%2F%2Fvirtual.ucsp.edu.pe%2Fadmin%2Foauth2callback.php&state=%2Fauth%2Foauth2%2Flogin.php%3Fwantsurl%3Dhttps%253A%252F%252Fvirtual.ucsp.edu.pe%252F%26sesskey%3DEt6pFTwPgM%26id%3D1&scope=openid%20profile%20email&flowName=GeneralOAuthFlow"
-    # virtual_ucsp_login_url: str = "https://virtual.ucsp.edu.pe/login/index.php"
+    virtual_ucsp_login_url: str = "https://virtual.ucsp.edu.pe/login/index.php"
 
     ***REMOVED***
       "id": "jose.vilca.campana@ucsp.edu.pe",
@@ -16,20 +59,21 @@ def main():
 # the anchor of each course is in div.card AND  data-type="1"
 
     with sync_playwright() as p:
-        # browser = p.chromium.launch(headless=False, slow_mo=50)
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=False, slow_mo=50)
+        # browser = p.chromium.launch()
         page = browser.new_page()
         page.goto(virtual_ucsp_login_url)
-        # page.click("a[title=Google]")
-        # html = page.inner_html("#identifierId")
-        html = page.inner_html("body")
-        # page.fill('#identifierId', user_info["id"])
-        # page.click("#identifierNext")
-        # page.fill('[name=password]', user_info["password"])
-        # page.click("#passwordNext")
-        # html = page.inner_html("li[data-key=mycourses")
+        page.click("a[title=Google]")
+
+        page.fill('input#identifierId', user_info["id"])
+        page.locator('text=Siguiente').click()
+        page.fill('input[type=password]', user_info["password"])
+        page.locator('text=Siguiente').click()
+        html = page.inner_html("li[data-key=mycourses]")
         print(html)
+        time.sleep(1000)
 
 
 if __name__ == "__main__":
-    main()
+    get_academico_course_weights()
+    # main()
